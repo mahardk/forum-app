3f8d74f302b4682046f9ac6d34e341806e688180
mahardk <zlfkrrvhx@gmail.com>
2026-03-15 16:20:05 +0700

    fix: add cypress globals and ignore config files in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import daStyle from 'eslint-config-dicodingacademy'
 
 export default [
-  { ignores: ['dist', 'node_modules'] },
+  { ignores: ['dist', 'node_modules', 'eslint.config.js', 'vite.config.js', 'cypress.config.js'] },
   js.configs.recommended,
   daStyle,
 
@@ -39,6 +39,24 @@ export default [
         args: 'after-used',
         ignoreRestSiblings: true,
       }],
+    },
+  },
+
+  {
+    files: ['cypress/**/*.js', 'cypress/**/*.cy.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        cy: 'readonly',
+        Cypress: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+        expect: 'readonly',
+      },
     },
   },
 ]
